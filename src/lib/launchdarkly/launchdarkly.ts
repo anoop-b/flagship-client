b5cbc626a89c2af51abd906bd8785fdd7d4d0b8e
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-12-28 00:23:06 +0530

    feat: added launchDarkly cached and cache invalidated variants

--- src/lib/launchdarkly/launchdarkly.ts
+++ src/lib/launchdarkly/launchdarkly.ts
@@ -1,30 +1,30 @@
 import { LD_SDK_KEY } from '$env/static/private';
-import LaunchDarkly from 'launchdarkly-node-server-sdk'
+import LaunchDarkly from 'launchdarkly-node-server-sdk';
 
-let launchDarklyClient: LaunchDarkly.LDClient
+let launchDarklyClient: LaunchDarkly.LDClient;
 
 async function initialize() {
-  const client = LaunchDarkly.init(LD_SDK_KEY)
-  try {
-    await client.waitForInitialization();
-  } catch (err) {
-    console.log("Failed to Init LD")
-  }
-  return client
+	const client = LaunchDarkly.init(LD_SDK_KEY);
+	try {
+		await client.waitForInitialization();
+	} catch (err) {
+		console.log('Failed to Init LD', err);
+	}
+	return client;
 }
 
-async function getClient() {
-  if (launchDarklyClient) return launchDarklyClient
-  return (launchDarklyClient = await initialize())
+export async function getClient() {
+	return await initialize();
 }
 
-export async function getFlagValue(key: string) {
-  const client = await getClient()
-  let flagValue
+export async function getClientWithCaching() {
+	if (launchDarklyClient) return launchDarklyClient;
+	return (launchDarklyClient = await initialize());
+}
 
-  const context: LaunchDarkly.LDContext = {
-    key: 'anonymous',
-  }
-  flagValue = await client.variation(key, context, false)
-  return flagValue
+export async function getFlagValue(client: LaunchDarkly.LDClient, key: string) {
+	const context: LaunchDarkly.LDContext = {
+		key: 'anonymous'
+	};
+	return await client.variation(key, context, false);
 }
--- src/routes/launchdarkly-ci/+page.server.ts
+++ src/routes/launchdarkly-ci/+page.server.ts
@@ -1,12 +1,13 @@
-import { getClientWithCaching, getFlagValue } from '$lib/launchdarkly/launchdarkly';
+import { getClient, getFlagValue } from '$lib/launchdarkly/launchdarkly';
 import type { PageServerLoad } from './$types';
 
 export const load = (async (event) => {
 	const name = event.url.searchParams.get('name') || 'ld';
 	const start = performance.now();
-	const client = await getClientWithCaching();
+	const client = await getClient();
 	const flag = await getFlagValue(client, 'isWinter');
 	const request_duration = (performance.now() - start).toPrecision(3);
+	client.close();
 
 	// Save duration to CSV without blocking
 	saveDurationToCSV(request_duration, name);
